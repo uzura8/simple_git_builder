6c1813f2083f4138cf09126f0c0ec19654a9f12b
Shingo Yamada <uzuranoie@gmail.com>
2019-03-22 00:04:35 +0900

    Implemented to change other months

--- src/js/templates/transactions.vue
+++ src/js/templates/transactions.vue
@@ -4,6 +4,36 @@
     Transactions
   </h1>
   <div v-if="transactions">
+    <nav class="pagination is-centered" role="navigation" aria-label="pagination">
+      <router-link :to="{path:'/transactions', query:{month: getMonth(1)}}" class="pagination-previous">
+        <b-icon pack="fas" icon="chevron-left"></b-icon>
+      </router-link>
+      <router-link :to="{path:'/transactions', query:{month: getMonth(-1)}}" class="pagination-next">
+        <b-icon pack="fas" icon="chevron-right"></b-icon>
+      </router-link>
+      <div class="pagination-list">
+        <div class="dropdown" :class="{ 'is-active': isActiveSelectMonth }">
+          <div class="dropdown-trigger">
+            <button class="button" aria-haspopup="true" aria-controls="dropdown-menu" @click="isActiveSelectMonth = !isActiveSelectMonth">
+              <span>{{ month }}</span>
+              <span class="icon is-small">
+                <i class="fas fa-angle-down" aria-hidden="true"></i>
+              </span>
+            </button>
+          </div>
+          <div class="dropdown-menu" id="dropdown-menu" role="menu">
+            <div class="dropdown-content">
+              <router-link
+                v-for="item in months" :key="item"
+                :to="{path:'/transactions', query:{month: item}}"
+                class="dropdown-item"
+                :class="{ 'is-active': month == item }"
+                v-text="item"></router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
     <table class="table">
       <thead>
         <tr>
@@ -32,18 +62,29 @@ import { moment } from '../bootstrap';
 export default {
   data(){
     return {
-      month: ''
+      months: [],
+      isActiveSelectMonth: false,
     }
   },
   computed: {
+    monthIndex () {
+      let queryMonth = this.$route.query.month;
+      if (!queryMonth) queryMonth = this.month = moment().format('YYYY-MM')
+      const index = this.months.indexOf(queryMonth)
+      this.loadTransactions(this.months[index])
+      this.isActiveSelectMonth = false
+      return index
+    },
+    month () {
+      return this.months[this.monthIndex]
+    },
     transactions () {
       return this.$store.state.transaction.list
       //return this.$store.getters.sortedTransactions
     }
   },
   created() {
-    if (!this.month) this.month = moment().format('YYYY-MM');
-    this.loadTransactions(this.month);
+    this.setMonths()
   },
   methods: {
     loadTransactions: function(month) {
@@ -51,7 +92,17 @@ export default {
         .catch(err => Promise.reject(err))
         .then(() => {
         })
-    }
+    },
+    setMonths: function() {
+      for (let i = 0, n = 12; i < n; i++) {
+        this.months.push(moment().add('months', -1 * i).endOf('month').format('YYYY-MM'))
+      }
+    },
+    getMonth: function(increment = 0) {
+      let index = this.monthIndex + increment
+      if (index < 0) index = this.months.length - 1
+      return this.months[index]
+    },
   }
 }
 </script>
