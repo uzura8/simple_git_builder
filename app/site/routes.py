14ecc1f99c10129622b0e7fd08ffc24f2f899b7c
Shingo Yamada <yamada-shingo@pal-relations.co.jp>
2019-08-16 01:35:44 +0000

    Added route for

--- app/site/routes.py
+++ app/site/routes.py
@@ -11,3 +11,9 @@ def before_request():
 @bp.route('/<path:path>')
 def catch_all(path):
     return render_template('index.html')
+
+
+## For Let's encrypt confirmation
+#@bp.route('/.well-known/acme-challenge/<filename>')
+#def well_known(filename):
+#    return render_template('.well-known/acme-challenge/'+ filename)
